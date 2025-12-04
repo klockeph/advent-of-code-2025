0815fbdf27d52bcc098084a8b9beac47ec5913fc
Philipp Klocke <philipp.a.klocke@gmail.com>
2025-12-04 10:22:51 +0100

    fix formatting

--- day03.rkt
+++ day03.rkt
@@ -10,9 +10,9 @@
   (cond
     [(= count 1) (digit-add acc (foldl max 0 digits))]
     [else
-      (define digits-length (length digits))
-      (define curr-digit-i (argmax-index identity (take digits (- digits-length (- count 1)))))
-      (max-num (- count 1) (drop digits (+ (cdr curr-digit-i) 1)) (digit-add acc (car curr-digit-i)))]))
+     (define digits-length (length digits))
+     (define curr-digit-i (argmax-index identity (take digits (- digits-length (- count 1)))))
+     (max-num (- count 1) (drop digits (+ (cdr curr-digit-i) 1)) (digit-add acc (car curr-digit-i)))]))
 
 (define (digit-value digit-char)
   (- (char->integer digit-char)
@@ -20,7 +20,7 @@
 
 (define (max-joltage line count)
   (max-num count (map digit-value (string->list line)))
-   )
+  )
 
 (define (solve count)
   (for/sum ([l input]) (max-joltage l count)))
